8782d2e0c1e60b8a3acd990db86337b950c9d480
GustavoGPL <gustavoguillan23@gmail.com>
2025-03-30 22:00:16 -0300

    Adjusting css

--- src/app/estatisticas/page.tsx
+++ src/app/estatisticas/page.tsx
@@ -4,7 +4,7 @@ import { useCronometroStore } from '@/hooks/useCronometro';
 
 export default function Estatisticas() {
 	return (
-		<div className="flex w-full items-center flex-col p-4 sm:p-10 h-screen">
+		<div className="flex w-full items-center flex-col p-4 sm:px-10 sm:py-5 h-screen overflow-auto ">
 			<h1 className="w-full text-2xl font-bold mb-8">Estatísticas</h1>
 			<section className="w-full border-[1px] border-slate-300 rounded-lg">
 				<div className="p-4">
